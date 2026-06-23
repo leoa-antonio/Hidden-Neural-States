549a42c01bce7350aab9c557d83a5e7a1f2b6acd
Leo Antonio <loa2011@nyu.edu>
2026-06-22 20:01:11 -0400

    Added dynamics.py and notebook folder

---
+++ src/dynamics.py
@@ -0,0 +1,27 @@
+import numpy as np;
+
+def simulate_linear_system(A, x0, T):
+    x_prev = x0;
+    X = np.zeros((T+1, x0.shape[0]))
+    X[0] = x0
+    for i in range(T):
+        x_next = A @ x_prev
+        X[i+1] = x_next
+        x_prev = x_next
+    return X;
+
+def rotation_matrix(theta):
+    return np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta), np.cos(theta)]
+    ]);
+
+def damped_rotation_matrix(theta, r):
+    if not (0 <= r <= 1):
+        raise ValueError("r must satisfy 0 <= r <= 1")
+    return r * rotation_matrix(theta)
+
+theta = 45
+r = 20
+print(damped_rotation_matrix(theta, r))
+
