dbd0e004913dfdd84435508b164119f00dd58e92
Leo Antonio <loa2011@nyu.edu>
2026-06-26 17:08:20 -0400

    Removed semicolons. Created simiulations.py

---
+++ src/simulations.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def generate_observation_matrix(n_neurons, latent_dim, scale=1.0):
+    C = np.random.randn(n_neurons, latent_dim)
+    return C * scale
+
+def stimulate_neural_activity(X, C):
+    return X @ C.T
+
+def add_observation_noise(Y, noise_std):
+    return Y + np.random.randn(*Y.shape) * noise_std
