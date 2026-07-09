122b58f40339e77b9e1b314346a3abcf1a75ffc2
Leo Antonio <loa2011@nyu.edu>
2026-07-09 19:53:33 -0400

    Implement PCA recovery

--- src/simulations.py
+++ src/simulations.py
@@ -1,11 +1,16 @@
 import numpy as np
 
-def generate_observation_matrix(n_neurons, latent_dim, scale=1.0):
-    C = np.random.randn(n_neurons, latent_dim)
+def generate_observation_matrix(n_neurons, latent_dim, scale=1.0, rng = None):
+    if rng == None:
+        rng = np.random.default_rng()
+
+    C = rng.standard_normal((n_neurons, latent_dim))
     return C * scale
 
 def simulate_neural_activity(X, C):
     return X @ C.T
 
-def add_observation_noise(Y, noise_std):
-    return Y + np.random.randn(*Y.shape) * noise_std
+def add_observation_noise(Y, noise_std, rng = None):
+    if rng == None:
+        rng = np.random.default_rng()
+    return Y + rng.standard_normal(Y.shape) * noise_std
